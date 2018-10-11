c43445b14afddfdeadc5f3076675c9e8fc1ee67c
Hobson Lane <github@totalgood.com>
2018-10-10 17:12:27 -0700

    fix doctests and comment out conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
     Dummy conftest.py for pugnlp.
@@ -7,6 +6,6 @@
     Read more about conftest.py under:
     https://pytest.org/latest/plugins.html
 """
-from __future__ import print_function, absolute_import, division
+# from __future__ import print_function, absolute_import, division
 
 # import pytest
